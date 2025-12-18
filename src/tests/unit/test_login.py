89ba9f84df6352d4d16347fe1d56cb9271636390
Aziz-Ahmd <azizahmd119@gmail.com>
2025-12-18 16:50:51 +0500

    Delete src directory

--- src/tests/unit/test_login.py
+++
@@ -1,7 +0,0 @@
-from src.login import login
-
-def test_valid_login():
-    assert login("admin", "1234") == "Login Successful"
-
-def test_invalid_password():
-    assert login("admin", "wrong") == "Login Failed"
